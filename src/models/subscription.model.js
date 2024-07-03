7fcc385e903f45853b2e7551602b0b4b91e2be90
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-07-03 13:25:21 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchem = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchem);
